68f1be47bc4277686328342f50a87aa1ebababc5
EMCTeamIR <EMCTeamIR@users.noreply.github.com>
2016-03-08 16:03:26 +0330

    Update commands.lua

--- plugins/commands.lua
+++ plugins/commands.lua
@@ -1,3 +1,5 @@
+do
+
 function run(msg, matches)
     local text = "#mute [username/reply]\n\n#unmute [username/reply]\n\n#kick [username/reply]\n\n#kickme\n\n#ban [username/reply]\n\n#set mod [username\reply]\n\n#set member [username/reply]\n\n#set admin [username/reply]\n\n#muteall\n\n#unmuteall\n\n"
     return text
